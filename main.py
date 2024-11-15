0ea58d3433d28fcdcd8cea3e87c4806a06189d89
kabust <tulyulyuk91@gmail.com>
2024-11-15 23:13:16 +0100

    added custom logger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import logging
 import sys
 
 from aiogram import Bot, Dispatcher, html
@@ -24,6 +23,7 @@ from db.user_handler import (
 from db.sent_ads_handler import write_ad, filter_ads, delete_old_records
 from scraper import get_last_50_items, verify_city
 from utils import are_cities_similar, remove_accents
+from logger import logger
 
 
 load_dotenv()
@@ -32,12 +32,6 @@ load_dotenv()
 TOKEN = os.getenv("API_TOKEN")
 dp = Dispatcher()
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-logging.basicConfig(
-    filename="app.log",
-    level=logging.INFO,
-    format="%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S:%MS"
-)
 
 
 class Form(StatesGroup):
@@ -47,13 +41,13 @@ class Form(StatesGroup):
 
 async def send_scheduled_message():
     while True:
-        logging.log(20, f"Deleting outdated saved ads")
+        logger.log(20, f"Deleting outdated saved ads")
         delete_old_records()
 
         users = get_all_users_with_city()
         unique_cities = get_unique_cities()
 
-        logging.log(
+        logger.log(
             20,
             f"Getting ads for {len(unique_cities)} cities and sending to {len(users)} users",
         )
@@ -66,10 +60,10 @@ async def send_scheduled_message():
             try:
                 await task
             except Exception as e:
-                logging.log(30, e)
+                logger.log(30, e)
         # await asyncio.gather(*tasks, return_exceptions=True)
 
-        logging.log(20, "All users were processed")
+        logger.log(20, "All users were processed")
         await asyncio.sleep(300)
 
 
@@ -95,7 +89,7 @@ async def send_items(user: tuple, items: dict) -> None:
         text = f"<strong><a href='{item_link}'>{title}</a></strong>\n \
         \n{price} | {size}\n{location} - Published at {publication_time}\n"
         await bot.send_message(user["chat_id"], text)
-    logging.log(20, f"Sent {ads_count} items for user {user['user_id']}")
+    logger.log(20, f"Sent {ads_count} items for user {user['user_id']}")
 
 
 @dp.message(CommandStart())
@@ -104,7 +98,7 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
     This handler receives messages with `/start` command
     """
     user_id = message.from_user.id
-    logging.log(20, f"Received /start from user {user_id}")
+    logger.log(20, f"Received /start from user {user_id}")
 
     user = get_user(user_id)
     if not user:
@@ -178,5 +172,5 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logger.basicConfig(level=logger.INFO, stream=sys.stdout)
     asyncio.run(main())
